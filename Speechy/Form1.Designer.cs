d47e96502663a29565b3aa7f05bbe7cf6e763656
cherybloo <laurenciomichael98@gmail.com>
2023-07-07 11:18:44 +0200

    Created C# WPF Application

---
+++ Speechy/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace turbo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.refreshBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(19, 13);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.Size = new System.Drawing.Size(498, 345);
+            this.outputBox.TabIndex = 0;
+            this.outputBox.TextChanged += new System.EventHandler(this.outputBox_TextChanged);
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Location = new System.Drawing.Point(538, 13);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(226, 198);
+            this.refreshBtn.TabIndex = 1;
+            this.refreshBtn.Text = "refresh";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.outputBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button refreshBtn;
+    }
+}
+
---
+++ Speechy/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace turbo
+{   
+    public partial class Form1 : Form
+    {
+        private const string URL = "http://localhost:8000/about"; //change your json link here
+
+        SpeechSynthesizer ss;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ss = new SpeechSynthesizer();
+            ss.Volume=100;
+        }
+
+        async Task<string> waitingBullshit()
+        {
+            using(WebClient client = new WebClient())
+            {
+                string userInput = "";
+                var response = await client.DownloadStringTaskAsync(URL);
+                dynamic dobj = JsonConvert.DeserializeObject<dynamic>(response);
+                userInput = dobj["user"];
+                return userInput;
+            }
+        }
+
+
+        async void Form1_Load(object sender, EventArgs e)
+        {
+            string prevInput = "";
+            while (true)
+            {
+                var response = await waitingBullshit();
+                if (prevInput != response)
+                {
+                    outputBox.Text = response;
+                    prevInput = response;
+                    response = "";
+                }
+            }
+           
+        }
+        
+        private void outputBox_TextChanged(object sender, EventArgs e)
+        {
+            if (outputBox.Text != "")
+            {
+                
+                ss.Speak(outputBox.Text);
+            }
+        }
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = "";
+        }
+    }
+}
